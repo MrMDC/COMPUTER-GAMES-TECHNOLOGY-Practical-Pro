1bf48c97361453981c435904e76a101294bd31a1
Douper Lim <weijie881@gmail.com>
2026-02-23 01:03:08 +0800

    Initial - P3

---
+++ Final - Practical 3 -Wander/Assets/Scripts/Ch01Behaviours/Evade.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Evade : Flee
+{
+    
+
+    public override void Awake()
+    {
+       
+    }
+
+  
+
+    void OnDestroy ()
+    {
+        
+    }
+}
