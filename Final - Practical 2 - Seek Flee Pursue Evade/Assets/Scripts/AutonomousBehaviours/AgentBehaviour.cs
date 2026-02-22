79db78d9e5482c90633b4b11ce7d7394d13cff13
Douper Lim <weijie881@gmail.com>
2026-02-23 01:02:58 +0800

    Initial - P2

---
+++ Final - Practical 2 - Seek Flee Pursue Evade/Assets/Scripts/AutonomousBehaviours/AgentBehaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+public class AgentBehaviour : MonoBehaviour
+{
+    public GameObject target;
+    protected Agent agent;
+    public virtual void Awake()
+    {
+        agent = gameObject.GetComponent<Agent>();
+    }
+    public virtual void Update()
+    {
+        agent.SetSteering(GetSteering());
+    }
+    public virtual Steering GetSteering()
+    {
+        return new Steering();
+    }
+
+    public Vector3 OriToVec (float orientation)
+    {
+        Vector3 vector = Vector3.zero;
+        vector.x = Mathf.Sin(orientation * Mathf.Deg2Rad) * 1.0f;
+        vector.z = Mathf.Cos(orientation * Mathf.Deg2Rad) * 1.0f;
+        return vector.normalized;
+    }
+}
