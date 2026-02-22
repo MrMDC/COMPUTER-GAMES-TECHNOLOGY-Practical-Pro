36cfbe93aa9f901d0edec3d6d27932a4a4d7d16c
Douper Lim <weijie881@gmail.com>
2026-02-23 01:04:12 +0800

    Initial - P7

---
+++ Final - Practical 7 - NavMesh/Assets/AIControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
